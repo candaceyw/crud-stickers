235276a78ac7056cde8210d8f0a45da10996ea8d
candaceyw <candaceyw@gmail.com>
2020-05-21 16:11:15 -0400

    adding testing

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,7 +1,10 @@
 module.exports = {
   development: {
     client: 'pg',
-    password: 'postgres',
     connection: 'postgres://postgres:Bokeh2470@localhost/cw-web-store',
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://postgres:Bokeh2470@localhost/test-cw-web-store',
   },
 };
